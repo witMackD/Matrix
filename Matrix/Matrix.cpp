a84ba024e0174492d109aae01e39e109f7a046e8
Maciej Drozdowicz <drozdom0@ad.wsisiz.edu.pl>
2019-12-07 19:58:17 +0100

    Konstruktory przypisujace i inne rzeczy

--- Matrix/Matrix.cpp
+++ Matrix/Matrix.cpp
@@ -8,8 +8,11 @@ using namespace std;
 class Matrix {
 public:
 	Matrix(int rs, int cs);
+	Matrix();
+	Matrix(const Matrix &m);
 	~Matrix();
 	double &operator()(int r, int c);
+	Matrix &operator = (const Matrix &m);
 	const double &operator () (int r, int c) const;
 private:
 	int rs, cs;
@@ -17,7 +20,21 @@ private:
 };
 
 Matrix::Matrix(int rs, int cs) : rs(rs), cs(cs), d(new double[rs * cs]) {
-std:cout << "Matrix(int, int)" << std::endl;
+	std:cout << "Matrix(int, int)" << std::endl;
+}
+
+Matrix::Matrix(): rs(), cs(), d()
+{
+	std:cout << "Matrix()" << std::endl;
+}
+
+Matrix::Matrix(const Matrix & m) : rs(m.rs), cs(m.cs), d(m.d)
+{
+std:cout << "Matrix(const Matrix &)" << std::endl;
+	for (int i = 0; i < rs*cs; ++i) {
+		d[i] = m.d[i];
+	}
+
 }
 
 Matrix::~Matrix() {
@@ -30,6 +47,17 @@ double &Matrix::operator () (int r, int c) {
 	return d[r*cs + c];
 }
 
+Matrix &Matrix::operator=(const Matrix & m)
+{
+	rs = m.rs;
+	cs = m.cs;
+	d = new double[rs*cs];
+	for (int i = 0; i < rs * cs; i++) {
+		d[i] = m.d[i];
+	}
+	return *this;
+}
+
 const double &Matrix::operator () (int r, int c) const {
 	return d[r*cs + c];
 }
@@ -37,7 +65,7 @@ const double &Matrix::operator () (int r, int c) const {
 int main()
 {
 	Matrix m1(2, 3);
+	Matrix m2 = m1;
 	m1(1, 2) = 7;
-	m1.~Matrix();
 	std::cout << m1(1, 2) << std::endl;
 }
